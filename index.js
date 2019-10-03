2f6a0c3ea06a12fc425f22b8658e5f6edf9865ad
Thomas Kottke <tkottke@witsmd.com>
2019-10-03 08:19:52 -0500

    feat:Added invalid command catch ## Git Commit Template v1.0 ## Author: Thomas Kottke <admin@tdkottke.com> ## Template supports the logging of information related to the ticket based on multiple git commit template philosophies
    # Files committed:
      - index.js
    
    # Ticketing System Information
      # ticket: <tracking ticket>
    
    # Commit Summery:
      Why is the change necessary?
        This commit adds a check that the user entered a valid commit.  If they did not, it returns basic information about the tool including the usage syntax and current commands
      How does it address the issue?
        Add a comparison to the inquirer module to see if the command exists, and responds accordingly
      What side effects does this change have?
        N/A

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const [,, command, ...args] = process.argv;
 const initialize = async () => {
 	const cli = await require('inquirer.module')();
 
-	cli.commit();
+	if (!cli[command]) {
+		console.log(`
+
+Invalid Command: ${command}
+		
+
+Usage: update-notes <command> [options]
+
+Commands:
+  exclude - add untracked files to local file exclusion .git/info/exclude
+  commit - cli interface for generating and committing files
+		`);
+		process.exit(0);
+	}
+
+	cli[command]();
 
 }
 
 console.clear();
+console.log('--- Note-CLI Tool ---')
 initialize();
